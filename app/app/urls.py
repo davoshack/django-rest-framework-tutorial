38c6d7ff3346e7ae6ae07374e3ce1ff9edaccbe2
Juan David H <juan.hernandez@anomali.com>
2021-07-29 21:27:42 -0500

    Added app

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,4 +1,4 @@
-"""quickapp URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,15 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from snippets import views
+from django.urls import path
 
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('snippets.urls')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
